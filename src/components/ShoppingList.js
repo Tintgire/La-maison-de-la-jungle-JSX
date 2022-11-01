f71ad021b79f2bc5e8a681966b442d2b2f427a5e
Tintgire <boidinhugo14@gmail.com>
2022-11-02 00:27:36 +0100

    plant emoji

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,18 +1,28 @@
-const plantList = [
-  "monstera",
-  "ficus lyrata",
-  "pothos argenté",
-  "yucca",
-  "palmier",
-];
+import { plantList } from "../datas/plantList";
 
 function ShoppingList() {
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
+
   return (
-    <ul>
-      {plantList.map((plant, index) => (
-        <li key={`${plant}-${index}`}>{plant}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>
+            {plant.name}
+            {plant.isBestSale && <span>🔥</span>}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
