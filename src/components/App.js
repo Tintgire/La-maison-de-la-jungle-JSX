05909d74ab0aef44eba3f7c1dc7d3071646b5b67
Tintgire <boidinhugo14@gmail.com>
2022-11-14 06:04:15 +0100

    div cart shop

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
-import QuestionForm from "./QuestionForm";
+import Footer from "./Footer";
+import "../styles/Layout.css";
 
 function App() {
   return (
     <div>
       <Banner />
-      <Cart />
-      <ShoppingList />
-      <QuestionForm />
+      <div className="lmj-layout-inner">
+        <Cart />
+        <ShoppingList />
+      </div>
+      <Footer />
     </div>
   );
 }
